bde862072baba1dfbce71b0c708e1debc8aeb24d
Hugh Ybarra <Hugh.ybarra@gmail.com>
2014-05-29 08:35:51 -0400

    got landing page working

--- fuel/app/views/welcome/index.php
+++ fuel/app/views/welcome/index.php
@@ -7,6 +7,7 @@
 	<!-- mayers reset -->
 	<?= Asset::css('reset.css') ?>
 	<?= Asset::css('bootstrap.css') ?>
+	<?= Asset::css('landing/main.css') ?>
 
 
 
@@ -14,54 +15,110 @@
 </head>
 <body>
 
-	<nav class="navbar navbar-inverse" role="navigation">
-		<div class="container">
-			 <a class="navbar-brand" href="#">Hugo's Workshop</a>
-		</div>
-	</nav>
-	<!-- end navigation -->
 
 	<div class="container">
 
-		<section class="jumbotron">
-		  <h1>Hugh Ybarra Portafolio Page</h1>
-		  <p>This site is dedicated to some of the work that I have built while going to school at Fullsail University. I hope you enjoy. </p>
+		<section class="jumbotron text-left">
+		  <h1>The Turnkey Web Engine</h1>
+		  <p>Hey how goes it? This is Hugh Ybarra here and you're looking at my FullSail Final Project. Feel Free to contact me
+		  	here <a href="">hugh.ybarra@gmail.com</a> with any of your questions. If your interested in my services feel free to send me
+		  	a message. Ill get back to you as soon as possible.</p>
 		</section>
 
 		<!-- end section -->
-		<section class="about">
-			<h2>Hey Hey this is Hugh Ybarra</h2>
-			<p>
-				feel free to have a look around my site and check out some of my <a href="#">recent projects</a> and a few of <a href="#">my personal projects</a> as well. Take a gander and if you're
-				interested in working to gether <a href="#">get in touch</a>
-			</p>
+		<section class="about row col-lg-12 ">
+
+			<div class="text col-lg-6">
+				<h3>So Whats this all about? </h3>
+				<p>
+					My project is a business model for restaurant website development. The goal is to speed up all aspects of development
+					when developing a site from the start to increase productivity for myself and my company. I plan on having a set of
+					predesigned solutions to pick from ranging from just website development with many templates and modules to pick
+					from, to a full online footprint, SEO plan, social media coverage strategy, a hosting plan, and  later a restaurant
+					management system for those who opt into it.
+				</p>
+			</div>
+
+			<div class="text col-lg-6">
+				<h3>Why did I build this? </h3>
+				<p>
+					The purpose of this templating system is to build a great set of template sites targeted at restaurant style websites
+					for rapid development and integration.  Ideally, once the project is planned and defined turnaround should be as quick
+					as possible.
+				</p>
+			</div>
+
+			<div class="text col-lg-6">
+				<h3> What is success? </h3>
+				<p>
+					A new website launching that is available on any device through the browser, searchable by all major search
+					engines and hosted with 99.999 % up time
+				</p>
+			</div>
+
+			<div class="text col-lg-6">
+				<h3>Required Content</h3>
+				<p>
+					Each new client that wants to use this engine will need these assets.
+					th
+					<ul>
+						<li>Nine Images</li>
+						<li>One Icon</li>
+						<li>Copy about the restaurant</li>
+						<li>Menus</li>
+						<li>An address Street, City, State</l1>
+						<li>Hours of operation</li>
+						<li>Contact Inforamtion</li>
+					</ul>
+
+					Once The client gives me this list and choses a template I can insert it into the data base and new website will be generated based off of a
+					pre selected template.
+				</p>
+			</div>
+
 		</section>
 
 
 		<section class="content">
 
+			<div class="examples">
+				<h2>The Examples</h2>
+				<p>
+					I would like to explain what you will be looking at here. Below are two restaurants PizzaJohns and Dr.Donut. Right now
+					both restaurants have their asssets stored in the database. PizzaJohns will be rendering template one, and Dr. Donut will be
+					rednering template two. Right now I only have two templates to chose from but you can swap out both restaurants and change
+					templates.
+				</p>
+			</div>
+
 			<div class="row featurette">
 
-				<div class="col-lg-7">
-					<h2 class="">Pizza Johns</h2>
-					<p class="lead">
-						Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
-					</p>
-				</div>
-				<div class="col-lg-5">
-					<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iI2VlZSI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjI1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojYWFhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjMxcHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NTAweDUwMDwvdGV4dD48L3N2Zz4=">
+				<div class="sub-section pizzajohns">
+					<div class="col-lg-7 feature-text text-left ">
+						<h3 class="">Pizza Johns</h3>
+						<p class="lead">
+							Pizza Johns was the first restaurant I used. Feel Free to check out template one with PizzaJohns restaurant as the filler content.
+						</p>
+					</div>
+
+					<div class="col-lg-5">
+						<?=  Asset::img('pizzajohns/logo.png', array('id' => 'logo')) ?>
+					</div>
 				</div>
 
-				<div class="col-lg-7">
-					<h2 class="">Dr Donut</h2>
-					<p class="lead">
-						Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.
-					</p>
-				</div>
-				<div class="col-lg-5">
-					<img src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI1MDAiIGhlaWdodD0iNTAwIj48cmVjdCB3aWR0aD0iNTAwIiBoZWlnaHQ9IjUwMCIgZmlsbD0iI2VlZSI+PC9yZWN0Pjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9IjI1MCIgeT0iMjUwIiBzdHlsZT0iZmlsbDojYWFhO2ZvbnQtd2VpZ2h0OmJvbGQ7Zm9udC1zaXplOjMxcHg7Zm9udC1mYW1pbHk6QXJpYWwsSGVsdmV0aWNhLHNhbnMtc2VyaWY7ZG9taW5hbnQtYmFzZWxpbmU6Y2VudHJhbCI+NTAweDUwMDwvdGV4dD48L3N2Zz4=">
-				</div>
+				<div class='sub-section drdonut'>
+					<div class="col-lg-7 feature-text">
+						<h3 class="">Dr Donut</h3>
+						<p class="lead">
+							Once I finished template 1 I needed a second restaurant to prove the concept. So I created Dr Donut. Have a look at Dr.Donut website.
+							This site is using Template two.
+						</p>
+					</div>
 
+					<div class="col-lg-5">
+						<?=  Asset::img('drdonut/logo.png', array('id' => 'logo')) ?>
+					</div>
+				</div>
 			</div>
 
 		</section>
@@ -70,7 +127,12 @@
 	<!-- end main container -->
 
 
+	<!-- Jquery -->
+	<script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+	<!-- Jquery Library -->
 
+	<!-- main.js -->
+	<?= Asset::js('landing/main.js', array(), null, true) ?>
 
 </body>
 </html>
---
+++ public/assets/js/landing/main.js
@@ -0,0 +1,15 @@
+$(function() {
+
+	$('.pizzajohns').click(function(){
+		window.location.href = 'pizzajohns';
+		event.preventDefault
+		return false;
+	});
+
+	$('.drdonut').click(function(){
+		window.location.href = 'drdonut';
+		event.preventDefault
+		return false;
+	});
+
+});
